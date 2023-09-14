7cccfbd781918fbc9c2c73f98f400ef1491e03be
Shimul <shimulmohammad420@gmail.com>
2023-09-14 17:26:55 +0600

    ok

--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -12,7 +12,7 @@ const Blogs = ({handleAddToBookmark,handleMarkAsRead}) => {
     }, [])
     return (
         <div className="md:w-2/3">
-            <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+            {/* <h1 className="text-4xl">Blogs: {blogs.length}</h1> */}
             {
                 blogs.map(blog => <Blog 
                     key={blog.id}
