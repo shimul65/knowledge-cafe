a382d0cd01834f701d7c4c6e4bfd2ab5bb9e1a05
Shimul <shimulmohammad420@gmail.com>
2023-09-14 15:32:21 +0600

    main section added

---
+++ src/Components/Blog/Blog.jsx
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types';
+
+const Blog = ({blog}) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+Blog.propTypes = { blog: PropTypes.object.isRequired}
+export default Blog;
---
+++ src/Components/Bookmarks/Bookmarks.jsx
@@ -0,0 +1,10 @@
+
+const Bookmarks = () => {
+    return (
+        <div className="md:w-1/3">
+            <h2 className="text-4xl">Bookmarks</h2>
+        </div>
+    );
+};
+
+export default Bookmarks;
